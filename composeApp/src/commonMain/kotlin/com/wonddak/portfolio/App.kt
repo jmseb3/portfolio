6ff47365be0a5198c646b037c58509951c6e102b
jmseb3 <jmseb3@naver.com>
2024-10-10 22:26:09 +0900

    Add Start Page

--- composeApp/src/commonMain/kotlin/com/wonddak/portfolio/App.kt
+++ composeApp/src/commonMain/kotlin/com/wonddak/portfolio/App.kt
@@ -1,11 +1,13 @@
 package com.wonddak.portfolio
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -87,12 +89,23 @@ internal fun App(
     ) { innerPadding ->
         NavHost(
             navController = navController,
-            startDestination = Screen.HOME.name,
+            startDestination = Screen.START.name,
             modifier = Modifier
                 .fillMaxSize()
                 .verticalScroll(rememberScrollState())
                 .padding(innerPadding)
         ) {
+            composable(Screen.START.name) {
+                Column {
+                    Button(
+                        onClick = {
+                            navController.navigate(Screen.HOME.name)
+                        }
+                    ) {
+                        Text("123")
+                    }
+                }
+            }
             composable(Screen.HOME.name) {
                 HomeView(mode, selectModel) { item ->
                     navController.navigate("${Screen.PROJECT.name}/${item.id}")
@@ -115,6 +128,7 @@ internal fun App(
 internal const val PROJECT_ID = "projectId"
 
 enum class Screen() {
+    START,
     HOME,
     PROJECT
 }
